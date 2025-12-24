65acaeb3601026aa841119b9f4201c5047310f2b
extg <extg@users.noreply.github.com>
2025-12-24 12:43:49 +0000

    feat: add ZodSerializerInterceptor and cleanupOpenApiDoc integration

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
+import { cleanupOpenApiDoc } from 'nestjs-zod';
 import { AppModule } from './app.module';
 import { appConfig } from './config';
 
@@ -29,7 +30,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, cleanupOpenApiDoc(document));
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? appConfig.port;
